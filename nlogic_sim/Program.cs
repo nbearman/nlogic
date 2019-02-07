7edc785cbe2e2c4b33c0b8b980544f2722c340af
Noah B <nobear123@live.com>
2019-02-06 20:41:39 -0500

    Interval tree class created for address lookup.

--- nlogic_sim/Program.cs
+++ nlogic_sim/Program.cs
@@ -11,7 +11,7 @@ namespace nlogic_sim
 {
     class Program
     {
-        static void Main(string[] args)
+        static void _Main(string[] args)
         {
             //generate_memory_test();
             //Console.ReadKey();
