33eb16325b51aca34317832c64e701e9042b66ab
Noah B <nobear123@live.com>
2019-09-15 20:50:15 -0400

    Began implementing READOUT formatting and added a visualizer definitions file.

--- nlogic_sim/processor/ProcessorVisualizer_New.cs
+++ nlogic_sim/processor/ProcessorVisualizer_New.cs
@@ -23,110 +23,6 @@ namespace nlogic_sim
             throw new NotImplementedException();
         }
 
-        //the location of the readout within the console window
-        private static Dictionary<READOUT, Tuple<int, int>> readout_coordinates = new Dictionary<READOUT, Tuple<int, int>>
-        {
-            { READOUT.Header, new Tuple<int, int>(0, 1) },
-            ////////////////////////////////////////////////////////////////////////////
-            { READOUT.FLAG, new Tuple<int, int>(4, 2) },
-            { READOUT.FLAG_contents, new Tuple<int, int>(12, 2) },
-            { READOUT.CurrentInstruction, new Tuple<int, int>(33, 2) },
-            { READOUT.CurrentInstruction_contents, new Tuple<int, int>(55, 2) },
-            { READOUT.CurrentInstruction_expansion, new Tuple<int, int>(65, 2) },
-            ////////////////////////////////////////////////////////////////////////////
-            { READOUT.EXE, new Tuple<int, int>(4, 5) },
-            { READOUT.EXE_contents, new Tuple<int, int>(12, 5) },
-            { READOUT.GPA, new Tuple<int, int>(35, 5) },
-            { READOUT.GPA_contents, new Tuple<int, int>(43, 5) },
-            { READOUT.ALUM, new Tuple<int, int>(66, 5) },
-            { READOUT.ALUM_contents, new Tuple<int, int>(74, 5) },
-            { READOUT.ALUM_expansion, new Tuple<int, int>(94, 5) },
-            ////////////////////////////////////////////////////////////////////////////
-            { READOUT.PC, new Tuple<int, int>(4, 6) },
-            { READOUT.PC_contents, new Tuple<int, int>(12, 6) },
-            { READOUT.GPB, new Tuple<int, int>(35, 6) },
-            { READOUT.GPB_contents, new Tuple<int, int>(43, 6) },
-            { READOUT.ALUA, new Tuple<int, int>(66, 6) },
-            { READOUT.ALUA_contents, new Tuple<int, int>(74, 6) },
-            { READOUT.ALUA_expansion, new Tuple<int, int>(94, 6) },
-            ////////////////////////////////////////////////////////////////////////////
-            { READOUT.GPC, new Tuple<int, int>(35, 7) },
-            { READOUT.GPC_contents, new Tuple<int, int>(43, 7) },
-            { READOUT.ALUB, new Tuple<int, int>(66, 7) },
-            { READOUT.ALUB_contents, new Tuple<int, int>(74, 7) },
-            { READOUT.ALUB_expansion, new Tuple<int, int>(94, 7) },
-            ////////////////////////////////////////////////////////////////////////////
-            { READOUT.SKIP, new Tuple<int, int>(4, 8) },
-            { READOUT.SKIP_contents, new Tuple<int, int>(12, 8) },
-            { READOUT.GPD, new Tuple<int, int>(35, 8) },
-            { READOUT.GPD_contents, new Tuple<int, int>(43, 8) },
-            { READOUT.ALUR, new Tuple<int, int>(66, 8) },
-            { READOUT.ALUR_contents, new Tuple<int, int>(74, 8) },
-            { READOUT.ALUR_expansion, new Tuple<int, int>(94, 8) },
-            ////////////////////////////////////////////////////////////////////////////
-            { READOUT.RTRN, new Tuple<int, int>(4, 9) },
-            { READOUT.RTRN_contents, new Tuple<int, int>(12, 9) },
-            ////////////////////////////////////////////////////////////////////////////
-            { READOUT.LINK, new Tuple<int, int>(4, 10) },
-            { READOUT.LINK_contents, new Tuple<int, int>(12, 10) },
-            { READOUT.GPE, new Tuple<int, int>(35, 10) },
-            { READOUT.GPE_contents, new Tuple<int, int>(43, 10) },
-            { READOUT.FPUM, new Tuple<int, int>(66, 10) },
-            { READOUT.FPUM_contents, new Tuple<int, int>(74, 10) },
-            { READOUT.FPUM_expansion, new Tuple<int, int>(94, 10) },
-            ////////////////////////////////////////////////////////////////////////////
-            { READOUT.GPF, new Tuple<int, int>(35, 11) },
-            { READOUT.GPF_contents, new Tuple<int, int>(43, 11) },
-            { READOUT.FPUA, new Tuple<int, int>(66, 11) },
-            { READOUT.FPUA_contents, new Tuple<int, int>(74, 11) },
-            { READOUT.FPUA_expansion, new Tuple<int, int>(94, 11) },
-            ////////////////////////////////////////////////////////////////////////////
-            { READOUT.GPG, new Tuple<int, int>(35, 12) },
-            { READOUT.GPG_contents, new Tuple<int, int>(43, 12) },
-            { READOUT.FPUB, new Tuple<int, int>(66, 11) },
-            { READOUT.FPUB_contents, new Tuple<int, int>(74, 12) },
-            { READOUT.FPUB_expansion, new Tuple<int, int>(94, 12) },
-            ////////////////////////////////////////////////////////////////////////////
-            { READOUT.COMPA, new Tuple<int, int>(4, 13) },
-            { READOUT.COMPA_contents, new Tuple<int, int>(12, 13) },
-            { READOUT.GPH, new Tuple<int, int>(35, 13) },
-            { READOUT.GPH_contents, new Tuple<int, int>(43, 13) },
-            { READOUT.FPUR, new Tuple<int, int>(66, 13) },
-            { READOUT.FPUR_contents, new Tuple<int, int>(74, 13) },
-            { READOUT.FPUR_expansion, new Tuple<int, int>(94, 13) },
-            ////////////////////////////////////////////////////////////////////////////
-            { READOUT.COMPB, new Tuple<int, int>(4, 14) },
-            { READOUT.COMPB_contents, new Tuple<int, int>(12, 14) },
-            ////////////////////////////////////////////////////////////////////////////
-            { READOUT.COMPR, new Tuple<int, int>(4, 15) },
-            { READOUT.COMPR_contents, new Tuple<int, int>(12, 15) },
-            ////////////////////////////////////////////////////////////////////////////
-            { READOUT.IADN, new Tuple<int, int>(4, 17) },
-            { READOUT.IADN_contents, new Tuple<int, int>(12, 17) },
-            ////////////////////////////////////////////////////////////////////////////
-            { READOUT.IADF, new Tuple<int, int>(4, 18) },
-            { READOUT.IADF_contents, new Tuple<int, int>(12, 18) },
-            ////////////////////////////////////////////////////////////////////////////
-            { READOUT.RBASE, new Tuple<int, int>(4, 22) },
-            { READOUT.RBASE_contents, new Tuple<int, int>(12, 22) },
-            { READOUT.MemoryContext1, new Tuple<int, int>(29, 22) },
-            { READOUT.WBASE, new Tuple<int, int>(58, 22) },
-            { READOUT.WBASE_contents, new Tuple<int, int>(66, 22) },
-            { READOUT.MemoryContext2, new Tuple<int, int>(82, 22) },
-            ////////////////////////////////////////////////////////////////////////////
-            { READOUT.ROFST, new Tuple<int, int>(4, 23) },
-            { READOUT.ROFST_contents, new Tuple<int, int>(12, 23) },
-            { READOUT.WOFST, new Tuple<int, int>(58, 23) },
-            { READOUT.WOFST_contents, new Tuple<int, int>(66, 23) },
-            ////////////////////////////////////////////////////////////////////////////
-            { READOUT.RMEM, new Tuple<int, int>(4, 24) },
-            { READOUT.RMEM_contents, new Tuple<int, int>(12, 24) },
-            { READOUT.WMEM, new Tuple<int, int>(58, 24) },
-            { READOUT.WMEM_contents, new Tuple<int, int>(66, 24) },
-
-
-        };
-
         //the current state of each readout
         private static Dictionary<READOUT, ColorString[]> readout_cache;
 
@@ -327,22 +223,60 @@ namespace nlogic_sim
                 case (READOUT.WBASE_contents):
                 case (READOUT.WMEM_contents):
                 case (READOUT.WOFST_contents):
-                    //standard register contents
-                    //split string into doublets separated by spaces
-                    //take a maximum of 4 doublets
-                    //pad string with 2 spaces on both sides
-                    //white when not "00 00 00 00"
-                    //dark gray otherwise
-                    break;
+                    {
+                        //standard register contents
+                        //take a maximum of 4 doublets
+                        //pad string with 2 spaces on both sides
+                        ColorString cs = new ColorString();
+                        cs.value = format_register(value, 4, 2, "  ", "  ");
+
+                        //white when not "  00 00 00 00  "
+                        if (cs.value == "  00 00 00 00  ")
+                            cs.color = ConsoleColor.White;
+                        //dark gray otherwise
+                        else
+                            cs.color = ConsoleColor.DarkGray;
+                        result_list.Add(cs);
+                        break;
+                    }
                 case (READOUT.CurrentInstruction_contents):
-                    //same as standard register contents
-                    //take only 2 doublets
-                    //TODO color instructions conditionally
-                    break;
+                    {
+                        //TODO color instructions conditionally
+
+                        //same as standard register contents
+                        //take only 2 doublets
+                        ColorString cs = new ColorString();
+                        cs.value = format_register(value, 2, 2, "  ", "  ");
+
+                        //white when not "  00 00  "
+                        if (cs.value == "  00 00  ")
+                            cs.color = ConsoleColor.White;
+                        //dark gray otherwise
+                        else
+                            cs.color = ConsoleColor.DarkGray;
+                        result_list.Add(cs);
+                        break;
+                    }
                 case (READOUT.CurrentInstruction_expansion):
-                    //pad string with 1 space to the left
-                    //change known register names to their corresponding colors
-                    break;
+                    {
+                        //TODO figure out how to format to include "->" and correct spacing
+                        //pad string with 1 space to the left
+                        string truncated = " " + value.Substring(0, 16);
+                        string[] separated = truncated.Split(' ');
+
+                        //create a ColorString for each section of the string
+                        foreach (string s in separated)
+                        {
+                            ColorString cs = new ColorString();
+                            cs.value = s;
+                            //color known register names with the corresponding color
+                            if (Processor.register_name_to_color.Keys.Contains(s))
+                                cs.color = Processor.register_name_to_color[s];
+                            else
+                                cs.color = ConsoleColor.Gray;
+                        }
+                        break;
+                    }
                 case (READOUT.ALUM_expansion):
                 case (READOUT.FPUM_expansion):
                     //pad string with 1 space on both sides
@@ -372,6 +306,34 @@ namespace nlogic_sim
         }
 
         /// <summary>
+        /// Changes the given string to be of the form "[padR]AB CD EF GH[padL]"
+        /// </summary>
+        /// <param name="value">String to be formatted</param>
+        /// <param name="num_groups">Number of groups of characters allowed</param>
+        /// <param name="group_size">Size of character groups in characters</param>
+        /// <param padL="padL">Padding for the left side</param>
+        /// <param name="padR">Padding for the right side</param>
+        /// <returns></returns>
+        private static string format_register(string value, int num_groups, int group_size, string padL, string padR)
+        {
+            //standard register contents
+            //take a maximum of 4 doublets
+            //pad string with 2 spaces on both sides
+            string truncated_string = value.Substring(0, Math.Min(value.Length, group_size * num_groups));
+            string result = padL;
+            for (int i = 0; i < truncated_string.Length; i++)
+            {
+                //split string into doublets separated by spaces
+                result += truncated_string[i];
+                if (i % group_size == 0 && i > 0)
+                    result += " ";
+            }
+            result += padR;
+
+            return result;
+        }
+
+        /// <summary>
         /// String with color information
         /// </summary>
         private struct ColorString
@@ -393,47 +355,6 @@ namespace nlogic_sim
 
 
         }
-
-        private enum READOUT
-        {
-            Header = 0,
-            FLAG = 1, FLAG_contents = 2,
-            CurrentInstruction, CurrentInstruction_contents, CurrentInstruction_expansion,
-            EXE, EXE_contents,
-            PC, PC_contents,
-            SKIP, SKIP_contents,
-            RTRN, RTRN_contents,
-            LINK, LINK_contents,
-            COMPA, COMPA_contents,
-            COMPB, COMPB_contents,
-            COMPR, COMPR_contents,
-            IADN, IADN_contents,
-            IADF, IADF_contents,
-            GPA, GPA_contents,
-            GPB, GPB_contents,
-            GPC, GPC_contents,
-            GPD, GPD_contents,
-            GPE, GPE_contents,
-            GPF, GPF_contents,
-            GPG, GPG_contents,
-            GPH, GPH_contents,
-            ALUM, ALUM_contents, ALUM_expansion,
-            ALUA, ALUA_contents, ALUA_expansion,
-            ALUB, ALUB_contents, ALUB_expansion,
-            ALUR, ALUR_contents, ALUR_expansion,
-            FPUM, FPUM_contents, FPUM_expansion,
-            FPUA, FPUA_contents, FPUA_expansion,
-            FPUB, FPUB_contents, FPUB_expansion,
-            FPUR, FPUR_contents, FPUR_expansion,
-            RBASE, RBASE_contents,
-            ROFST, ROFST_contents,
-            RMEM, RMEM_contents,
-            WBASE, WBASE_contents,
-            WOFST, WOFST_contents,
-            WMEM, WMEM_contents,
-            MemoryContext1,
-            MemoryContext2,
-        };
 
         /// <summary>
         /// Print the skeleton of the display to the console
@@ -465,9 +386,6 @@ namespace nlogic_sim
             Console.WriteLine("  ||       |               ||                           ||       |               ||");
             Console.WriteLine("  ||       |               ||                           ||       |               ||");
             Console.WriteLine("  ||       |               ||                           ||       |               ||");
-
-
-            throw new NotImplementedException();
         }
     }
 }
---
+++ nlogic_sim/processor/VisualizerDefinitions.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace nlogic_sim
+{
+    public partial class Processor
+    {
+        private enum READOUT
+        {
+            Header = 0,
+            FLAG = 1, FLAG_contents = 2,
+            CurrentInstruction, CurrentInstruction_contents, CurrentInstruction_expansion,
+            EXE, EXE_contents,
+            PC, PC_contents,
+            SKIP, SKIP_contents,
+            RTRN, RTRN_contents,
+            LINK, LINK_contents,
+            COMPA, COMPA_contents,
+            COMPB, COMPB_contents,
+            COMPR, COMPR_contents,
+            IADN, IADN_contents,
+            IADF, IADF_contents,
+            GPA, GPA_contents,
+            GPB, GPB_contents,
+            GPC, GPC_contents,
+            GPD, GPD_contents,
+            GPE, GPE_contents,
+            GPF, GPF_contents,
+            GPG, GPG_contents,
+            GPH, GPH_contents,
+            ALUM, ALUM_contents, ALUM_expansion,
+            ALUA, ALUA_contents, ALUA_expansion,
+            ALUB, ALUB_contents, ALUB_expansion,
+            ALUR, ALUR_contents, ALUR_expansion,
+            FPUM, FPUM_contents, FPUM_expansion,
+            FPUA, FPUA_contents, FPUA_expansion,
+            FPUB, FPUB_contents, FPUB_expansion,
+            FPUR, FPUR_contents, FPUR_expansion,
+            RBASE, RBASE_contents,
+            ROFST, ROFST_contents,
+            RMEM, RMEM_contents,
+            WBASE, WBASE_contents,
+            WOFST, WOFST_contents,
+            WMEM, WMEM_contents,
+            MemoryContext1,
+            MemoryContext2,
+        };
+
+        private static Dictionary<READOUT, Tuple<int, int>> readout_coordinates = new Dictionary<READOUT, Tuple<int, int>>
+        {
+            { READOUT.Header, new Tuple<int, int>(0, 1) },
+            ////////////////////////////////////////////////////////////////////////////
+            { READOUT.FLAG, new Tuple<int, int>(4, 2) },
+            { READOUT.FLAG_contents, new Tuple<int, int>(12, 2) },
+            { READOUT.CurrentInstruction, new Tuple<int, int>(33, 2) },
+            { READOUT.CurrentInstruction_contents, new Tuple<int, int>(55, 2) },
+            { READOUT.CurrentInstruction_expansion, new Tuple<int, int>(65, 2) },
+            ////////////////////////////////////////////////////////////////////////////
+            { READOUT.EXE, new Tuple<int, int>(4, 5) },
+            { READOUT.EXE_contents, new Tuple<int, int>(12, 5) },
+            { READOUT.GPA, new Tuple<int, int>(35, 5) },
+            { READOUT.GPA_contents, new Tuple<int, int>(43, 5) },
+            { READOUT.ALUM, new Tuple<int, int>(66, 5) },
+            { READOUT.ALUM_contents, new Tuple<int, int>(74, 5) },
+            { READOUT.ALUM_expansion, new Tuple<int, int>(94, 5) },
+            ////////////////////////////////////////////////////////////////////////////
+            { READOUT.PC, new Tuple<int, int>(4, 6) },
+            { READOUT.PC_contents, new Tuple<int, int>(12, 6) },
+            { READOUT.GPB, new Tuple<int, int>(35, 6) },
+            { READOUT.GPB_contents, new Tuple<int, int>(43, 6) },
+            { READOUT.ALUA, new Tuple<int, int>(66, 6) },
+            { READOUT.ALUA_contents, new Tuple<int, int>(74, 6) },
+            { READOUT.ALUA_expansion, new Tuple<int, int>(94, 6) },
+            ////////////////////////////////////////////////////////////////////////////
+            { READOUT.GPC, new Tuple<int, int>(35, 7) },
+            { READOUT.GPC_contents, new Tuple<int, int>(43, 7) },
+            { READOUT.ALUB, new Tuple<int, int>(66, 7) },
+            { READOUT.ALUB_contents, new Tuple<int, int>(74, 7) },
+            { READOUT.ALUB_expansion, new Tuple<int, int>(94, 7) },
+            ////////////////////////////////////////////////////////////////////////////
+            { READOUT.SKIP, new Tuple<int, int>(4, 8) },
+            { READOUT.SKIP_contents, new Tuple<int, int>(12, 8) },
+            { READOUT.GPD, new Tuple<int, int>(35, 8) },
+            { READOUT.GPD_contents, new Tuple<int, int>(43, 8) },
+            { READOUT.ALUR, new Tuple<int, int>(66, 8) },
+            { READOUT.ALUR_contents, new Tuple<int, int>(74, 8) },
+            { READOUT.ALUR_expansion, new Tuple<int, int>(94, 8) },
+            ////////////////////////////////////////////////////////////////////////////
+            { READOUT.RTRN, new Tuple<int, int>(4, 9) },
+            { READOUT.RTRN_contents, new Tuple<int, int>(12, 9) },
+            ////////////////////////////////////////////////////////////////////////////
+            { READOUT.LINK, new Tuple<int, int>(4, 10) },
+            { READOUT.LINK_contents, new Tuple<int, int>(12, 10) },
+            { READOUT.GPE, new Tuple<int, int>(35, 10) },
+            { READOUT.GPE_contents, new Tuple<int, int>(43, 10) },
+            { READOUT.FPUM, new Tuple<int, int>(66, 10) },
+            { READOUT.FPUM_contents, new Tuple<int, int>(74, 10) },
+            { READOUT.FPUM_expansion, new Tuple<int, int>(94, 10) },
+            ////////////////////////////////////////////////////////////////////////////
+            { READOUT.GPF, new Tuple<int, int>(35, 11) },
+            { READOUT.GPF_contents, new Tuple<int, int>(43, 11) },
+            { READOUT.FPUA, new Tuple<int, int>(66, 11) },
+            { READOUT.FPUA_contents, new Tuple<int, int>(74, 11) },
+            { READOUT.FPUA_expansion, new Tuple<int, int>(94, 11) },
+            ////////////////////////////////////////////////////////////////////////////
+            { READOUT.GPG, new Tuple<int, int>(35, 12) },
+            { READOUT.GPG_contents, new Tuple<int, int>(43, 12) },
+            { READOUT.FPUB, new Tuple<int, int>(66, 11) },
+            { READOUT.FPUB_contents, new Tuple<int, int>(74, 12) },
+            { READOUT.FPUB_expansion, new Tuple<int, int>(94, 12) },
+            ////////////////////////////////////////////////////////////////////////////
+            { READOUT.COMPA, new Tuple<int, int>(4, 13) },
+            { READOUT.COMPA_contents, new Tuple<int, int>(12, 13) },
+            { READOUT.GPH, new Tuple<int, int>(35, 13) },
+            { READOUT.GPH_contents, new Tuple<int, int>(43, 13) },
+            { READOUT.FPUR, new Tuple<int, int>(66, 13) },
+            { READOUT.FPUR_contents, new Tuple<int, int>(74, 13) },
+            { READOUT.FPUR_expansion, new Tuple<int, int>(94, 13) },
+            ////////////////////////////////////////////////////////////////////////////
+            { READOUT.COMPB, new Tuple<int, int>(4, 14) },
+            { READOUT.COMPB_contents, new Tuple<int, int>(12, 14) },
+            ////////////////////////////////////////////////////////////////////////////
+            { READOUT.COMPR, new Tuple<int, int>(4, 15) },
+            { READOUT.COMPR_contents, new Tuple<int, int>(12, 15) },
+            ////////////////////////////////////////////////////////////////////////////
+            { READOUT.IADN, new Tuple<int, int>(4, 17) },
+            { READOUT.IADN_contents, new Tuple<int, int>(12, 17) },
+            ////////////////////////////////////////////////////////////////////////////
+            { READOUT.IADF, new Tuple<int, int>(4, 18) },
+            { READOUT.IADF_contents, new Tuple<int, int>(12, 18) },
+            ////////////////////////////////////////////////////////////////////////////
+            { READOUT.RBASE, new Tuple<int, int>(4, 22) },
+            { READOUT.RBASE_contents, new Tuple<int, int>(12, 22) },
+            { READOUT.MemoryContext1, new Tuple<int, int>(29, 22) },
+            { READOUT.WBASE, new Tuple<int, int>(58, 22) },
+            { READOUT.WBASE_contents, new Tuple<int, int>(66, 22) },
+            { READOUT.MemoryContext2, new Tuple<int, int>(82, 22) },
+            ////////////////////////////////////////////////////////////////////////////
+            { READOUT.ROFST, new Tuple<int, int>(4, 23) },
+            { READOUT.ROFST_contents, new Tuple<int, int>(12, 23) },
+            { READOUT.WOFST, new Tuple<int, int>(58, 23) },
+            { READOUT.WOFST_contents, new Tuple<int, int>(66, 23) },
+            ////////////////////////////////////////////////////////////////////////////
+            { READOUT.RMEM, new Tuple<int, int>(4, 24) },
+            { READOUT.RMEM_contents, new Tuple<int, int>(12, 24) },
+            { READOUT.WMEM, new Tuple<int, int>(58, 24) },
+            { READOUT.WMEM_contents, new Tuple<int, int>(66, 24) },
+
+
+        };
+
+        /// <summary>
+        /// Mapping of register short names to visualizer colors
+        /// </summary>
+        private static Dictionary<string, ConsoleColor> register_name_to_color = new Dictionary<string, ConsoleColor>
+        {
+            {"FLAG", ConsoleColor.Green },
+            {"EXE", ConsoleColor.DarkYellow },
+            {"PC", ConsoleColor.White },
+            {"SKIP", ConsoleColor.Gray },
+            {"RTRN", ConsoleColor.White },
+            {"LINK", ConsoleColor.Cyan },
+            {"COMPA", ConsoleColor.DarkBlue},
+            {"COMPB", ConsoleColor.DarkMagenta },
+            {"COMPR", ConsoleColor.DarkGreen },
+            {"IADN", ConsoleColor.Red },
+            {"IADF", ConsoleColor.Green },
+            {"GPA", ConsoleColor.DarkGray },
+            {"GPB", ConsoleColor.Gray },
+            {"GPC", ConsoleColor.White },
+            {"GPD", ConsoleColor.Blue },
+            {"GPE", ConsoleColor.Cyan },
+            {"GPF", ConsoleColor.Green },
+            {"GPG", ConsoleColor.Yellow },
+            {"GPH", ConsoleColor.Red },
+            {"ALUM", ConsoleColor.DarkMagenta },
+            {"ALUA", ConsoleColor.DarkCyan },
+            {"ALUB", ConsoleColor.DarkCyan },
+            {"ALUR", ConsoleColor.Blue },
+            {"FPUM", ConsoleColor.DarkMagenta },
+            {"FPUA", ConsoleColor.DarkCyan },
+            {"FPUB", ConsoleColor.DarkCyan },
+            {"FPUR", ConsoleColor.Magenta },
+            {"RBASE", ConsoleColor.Yellow },
+            {"ROFST", ConsoleColor.Yellow },
+            {"RMEM", ConsoleColor.Green },
+            {"WBASE", ConsoleColor.Yellow },
+            {"WOFST", ConsoleColor.Yellow },
+            {"WMEM", ConsoleColor.Green },
+
+        };
+
+    }
+}
