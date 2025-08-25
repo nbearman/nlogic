161b78b059106da0b35f95c4a93801bf637ec9e0
nbearman <nbearman@umich.edu>
2025-08-24 19:32:56 -0700

    write out algorithm for page eviction in order to see it in code

--- nlogic_sim/tools/OS_Simulator/eviction.py
+++ nlogic_sim/tools/OS_Simulator/eviction.py
@@ -6,12 +6,34 @@ class PageType(Enum):
     PageTable = 2
     PageDirectory = 3
 
+class PageTableEntry:
+    readable: bool
+    write_protected: bool
+    number: int
+    original_pte_data: int
+
+    def __init__(self, data: int):
+        self.readable = 0b0 != (data & (0b1 << 31))
+        self.write_protected = 0b0 != (data & (0b1 << 30))
+        self.number = data & 0x000FFFFF
+        self.original_pte_data = data
+
+    def set_readable(self, value: bool):
+        pass
+
+    def set_write_protected(self, value: bool):
+        pass
+
+    def to_int(self) -> int:
+        pass
+
 @dataclass
 class ProcessMapEntry:
     pid: int
     num_resident_pages: int
     page_directory_block: int
     page_directory_ppage: int
+    disk_blocks: list[int]
 
 @dataclass
 class PPageMapEntry:
@@ -20,6 +42,8 @@ class PPageMapEntry:
     accessed: bool
     dirty: bool
     wired: bool
+    share_count: int
+    disk_block_number: int
 
 @dataclass
 class PageReference:
@@ -38,11 +62,18 @@ class Kernel:
         self.physical_page_map: list[PPageMapEntry] = []
         self.reference_list: list[PageReference] = []
         self.clock_hand: int = 0
+        self.disk_block_map: list[int] = [0] * 0x20 # tracks the share count of the disk blocks in swap space; a count of 0 is a free block
 
     def read_memory(self, address: int) -> int:
         pass
 
     def write_memory(self, address: int, value: int):
+        pass
+
+    def find_open_block(self) -> int:
+        pass
+
+    def copy_page_to_disk(self, ppage: int, disk_block: int):
         pass
 
     def set_not_readable(self, table_ppage: int, entry_number: int):
@@ -81,6 +112,7 @@ class Kernel:
 
         if ppage_entry.wired:
             # certain kernel pages can never be evicted
+            # other pages may be pinned, too, for e.g. for an asynchronous operation
             return False
 
         if ppage_entry.page_type is PageType.LeafPage:
@@ -92,8 +124,12 @@ class Kernel:
         # scan page references for any pages that point back to this one as the owning page or directory
         for reference in self.reference_list:
             if reference.table_ppage == ppage:
-                # there is a process that references a page in memory whose access is controlled by this table
-                return False
+                child_page_entry = self.physical_page_map[reference.ppage]
+                if child_page_entry.share_count <= 1:
+                    # there is a process that references a page in memory whose access is controlled by this table
+                    # and that page is not a shared page
+                    return False
+                # else, we can evict a table it if only points to shared pages (but we must mark PTEs to shared pages as non-resident before evicting this table)
 
         return True
 
@@ -110,10 +146,80 @@ class Kernel:
                 self.clear_access(self.clock_hand)
         return target_ppage
 
+    def evict_page_directory(self, ppage: int):
+        pass
+
 
     def evict_page(self, ppage: int):
-        # clear all references to this page
-        raise NotImplementedError("TODO implement this this")
+        ppage_entry = self.physical_page_map[ppage]
+
+        if ppage_entry.page_type is PageType.PageDirectory:
+            # evicting a directory has different logic because it has no PTE to update and its backing block is located elsewhere
+            # since page tables cannot be shared, we don't need to worry about if the PDEs point to shared pages
+            self.evict_page_directory(ppage)
+            return
+
+        elif ppage_entry.page_type is PageType.PageTable:
+            # if this is a page table, remove any mapped references to shared pages, because they may no longer be resident when
+            # this table comes back in. All other PTEs should already be marked non-resident for this table to have been considered evictable
+            page_base_addr = 0x1000 * ppage
+            for pte_offset in range(0x00, 0x1000, 0x04):
+                pte_addr = page_base_addr + pte_offset
+                pte = PageTableEntry(self.read_memory(pte_addr))
+                if pte.readable: # if the page is readable it must be mapped to a physical page
+                    refd_ppage = self.physical_page_map[pte.number]
+                    if refd_ppage.share_count <= 1:
+                        raise Exception("Attempted to evict a page table before all (non-shared) referenced pages have been paged out")
+                    pte.set_readable(False)
+                    pte.set_write_protected(True)
+                    self.write_memory(pte_addr, pte.to_int())
+                    ppage_entry.dirty = True # set the table we're about to evict as dirty, since we just modified one of its PTEs
+
+        elif ppage_entry.page_type is PageType.LeafPage:
+            # nothing specific to do for evicting a leaf page
+            pass
+
+        else:
+            raise ValueError()
+
+        if ppage_entry.dirty:
+            # physical page is dirty; it needs to be written back to disk
+            backing_block_is_shared = self.disk_block_map[ppage_entry.disk_block_number] > ppage_entry.share_count
+            # if the backing block is shared by only as many processes as share this physical page, we can continue to reuse that block
+            # otherwise, we will need a new block to avoid overwriting shared memory that was paged out
+            if backing_block_is_shared:
+                if ppage_entry.page_type is not PageType.LeafPage:
+                    raise Exception("Only leaf pages should be shared and have shared backing blocks.")
+                # we are going to allocate a new disk block to use
+                # remove the referencers pointing to this ppage from the disk block share count
+                self.disk_block_map[ppage_entry.disk_block_number] -= ppage_entry.share_count
+                open_block = self.find_open_block()
+                self.disk_block_map[open_block] += ppage_entry.share_count
+                ppage_entry.disk_block_number = open_block # store the newly allocated disk block in the ppage map entry; it only needs to be there for write back
+
+        # update all the PDEs/PTEs that point to this ppage to hold the new disk block and be non-resident
+        # (those tables' pages must also be marked dirty, since we're updating them)
+        references_to_remove = []
+        for reference in self.reference_list:
+            if reference.ppage == ppage:
+                # get the PDE/PTE referring to this ppage
+                entry_addr = (reference.table_ppage * 0x1000) + (reference.vpage * 0x04)
+                entry = PageTableEntry(self.read_memory(entry_addr))
+                # update the disk block and mark it as non-resident (not readable, write protected)
+                entry.number = ppage_entry.disk_block_number
+                entry.set_readable(False)
+                entry.set_write_protected(True)
+                self.write_memory(entry_addr, entry.to_int())
+                self.physical_page_map[reference.table_ppage].dirty = True
+                references_to_remove.append(reference)
+
+        for reference in references_to_remove:
+            self.reference_list.remove(reference)
+
+        if ppage_entry.dirty:
+            self.copy_page_to_disk(ppage, ppage_entry.disk_block_number)
+
+        return
 
 
     def handler(self):
